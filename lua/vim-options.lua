d5d9536ef0c1ba73504ae8704d15ab13e11c107d
ilDrugoLebiaski <biagio.cosentino96@gmail.com>
2026-01-28 12:45:06 +0100

    feat: keymaps to move between nvim splits

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,6 +1,13 @@
-
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=3")
 vim.cmd("set softtabstop=3")
 vim.cmd("set shiftwidth=3")
 vim.g.mapleader = " "
+
+vim.opt.swapfile = false
+vim.opt.number = true
+
+vim.keymap.set("n", "<c-h>", "<C-w>h")
+vim.keymap.set("n", "<c-j>", "<C-w>j")
+vim.keymap.set("n", "<c-k>", "<C-w>k")
+vim.keymap.set("n", "<c-l>", "<C-w>l")
