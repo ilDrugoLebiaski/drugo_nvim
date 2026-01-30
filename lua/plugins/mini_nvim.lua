447370b0a0b157e607c1af34155e194f7904efac
ilDrugoLebiaski <biagio.cosentino96@gmail.com>
2026-01-30 11:03:24 +0100

    feat: add smooth animations

--- lua/plugins/mini_nvim.lua
+++ lua/plugins/mini_nvim.lua
@@ -1,3 +1,58 @@
 return {
-  { "nvim-mini/mini.icons", version = "*" },
+  {
+    -- Mini.icons
+
+    "nvim-mini/mini.icons",
+    version = "*",
+    config = function()
+      require("mini.icons").setup({
+        style = "glyph", -- 'glyph' | 'ascii'
+
+        default = {},
+        directory = {},
+        extension = {},
+        file = {},
+        filetype = {},
+        lsp = {},
+        os = {},
+      })
+    end,
+  },
+
+  -- Mini.animate
+  {
+    "nvim-mini/mini.animate",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+      local animate = require("mini.animate")
+
+      animate.setup({
+        cursor = {
+          enable = true,
+          timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
+        },
+
+        scroll = {
+          enable = true,
+          timing = animate.gen_timing.linear({ duration = 50, unit = "total" }),
+        },
+
+        resize = {
+          enable = true,
+          timing = animate.gen_timing.linear({ duration = 50, unit = "total" }),
+        },
+
+        open = {
+          enable = true,
+          timing = animate.gen_timing.linear({ duration = 150, unit = "total" }),
+        },
+
+        close = {
+          enable = true,
+          timing = animate.gen_timing.linear({ duration = 150, unit = "total" }),
+        },
+      })
+    end,
+  },
 }
