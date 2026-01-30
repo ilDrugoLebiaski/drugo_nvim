2caca570246eccc8598d5b2d47cd5170900ceedc
ilDrugoLebiaski <biagio.cosentino96@gmail.com>
2026-01-30 11:04:47 +0100

    minor adjustments

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,13 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    opts = {
-        ensure_installed = {"c", "lua", "java", "javascript", "python"},
-        highlight = {enable=true},
-        indent = {enable = true},
-        auto_install = true
-    },
-    config = function(_, opts)
-        local config = require("nvim-treesitter").setup(opts)
-    end
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  opts = {
+    ensure_installed = { "c", "lua", "java", "javascript", "python" },
+    highlight = { enable = true },
+    indent = { enable = true },
+    auto_install = true,
+  },
+  config = function(_, opts)
+    local config = require("nvim-treesitter").setup(opts)
+  end,
 }
